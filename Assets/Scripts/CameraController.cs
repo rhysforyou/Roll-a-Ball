9619024ec16d7faa3ade052ba50eece0a8adc192
Rhys Powell <rhys@rpowell.me>
2018-01-22 15:08:43 +1100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float smoothTime = 0.3f;
+
+    private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
+	}
+
+    private void LateUpdate()
+    {
+        var targetPosition = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text scoreDisplay;
+
+    private Rigidbody rb;
+    private int score;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            RedrawScore();
+        }
+    }
+
+    private void RedrawScore() {
+        scoreDisplay.text = "Score: " + score;
+    }
+}
